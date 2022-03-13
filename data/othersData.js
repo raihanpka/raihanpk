0eab705dc3c36eaff35e35fc06e8eae33fcf49ec
Lunareix <me@raihanpk.com>
2022-03-14 00:48:46 +0700

    add donate link

--- data/othersData.js
+++ data/othersData.js
@@ -1,9 +1,15 @@
 const othersData = [
   {
     title: 'Server Discord',
-    description: `Server yang dikelola langsung oleh saya sendiri. Server yang biasanya saya gunakan untuk diskusi, santai-santai, dan bermain game bersama teman saya. Untuk saat ini tautannya belum bisa digunakan.`,
-    imgSrc: '/static/img/discord-logo.png',
+    description: `Server yang dikelola langsung oleh saya sendiri. Server yang biasanya saya gunakan untuk diskusi, santai-santai, dan bermain game bersama teman saya.`,
+    imgSrc: '/static/img/discord.png',
     href: 'https://dsc.gg/sundaempire',
+  },
+  {
+    title: 'Donate',
+    description: `Traktir saya kopi di sini. Bisa pakai Gopay, OVO, DANA, dan lainnya, loh.`,
+    imgSrc: '/static/img/saweria.png',
+    href: 'https://saweria.co/lunareix',
   },
 ]
 
