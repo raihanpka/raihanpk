a5bc456db69853e500a9008e168f472f25c214cb
Lunareix <me@raihanpk.com>
2023-01-03 22:17:39 +0700

    Update othersData.js

--- data/othersData.js
+++ data/othersData.js
@@ -1,11 +1,5 @@
 const othersData = [
   {
-    title: 'Server Discord',
-    description: `Server yang dikelola langsung oleh saya sendiri. Server yang biasanya saya gunakan untuk diskusi, santai-santai, dan bermain game bersama teman saya.`,
-    imgSrc: '/static/img/discord.png',
-    href: 'https://dsc.gg/sundaempire',
-  },
-  {
     title: 'Donate',
     description: `Traktir saya kopi di sini. Bisa pakai Gopay, OVO, DANA, dan lainnya, loh.`,
     imgSrc: '/static/img/saweria.png',
